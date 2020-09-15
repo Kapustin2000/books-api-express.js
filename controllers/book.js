9a0f83b185377320479c78a393b4a5499d1eb18d
Mikhail Kapustin <kapustinomm@gmail.com>
2020-09-15 16:51:24 +0300

    first commit

---
+++ controllers/book.js
@@ -0,0 +1,15 @@
+exports.index = function (req, res, next) {
+   console.log(res);
+};
+
+exports.show = function (req, res, next) {
+    console.log(res);
+};
+
+exports.store = function (req, res, next) {
+    console.log(res);
+};
+
+exports.update = function (req, res, next) {
+    console.log(res);
+};
---
+++ routes/books.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/book');
+
+/* GET users listing. */
+router.get('/', controller.index);
+router.get('/:book_id', controller.index);
+router.post('/', controller.index);
+router.post('/:book_id', controller.index);
+
+
+module.exports = router;
