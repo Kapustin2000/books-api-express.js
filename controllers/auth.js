7d3abd0beeeebe4f1ed08e3c1f6fbb33f5235c22
micle <Kapustin2000>
2020-09-28 15:05:49 +0300

    todo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 var User = require('../models/User');
 const {LoginValidation, RegisterValidation} = require('../validators/auth');
+const bcrypt = require('bcryptjs');
 
 
 exports.auth = function async (req, res, next) {
@@ -23,13 +24,15 @@ exports.register = async function (req, res, next) {
     const { error } = RegisterValidation(res.body);
     if(error) return res.status(400).send(error.details[0].message);
 
-    const userExists = User.findOne({email: req.body.email});
+    const userExists = await User.findOne({email: req.body.email});
     if(userExists) return res.status(400).send("Email already has been taken");
+
+    const salt = await bcrypt.genSalt(10);
 
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: await bcrypt.hash(req.body.password, salt)
     });
 
     try {
